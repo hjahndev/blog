43f4e2d6b6073a5f494682f18028568073e7a21a
hjahndev <hjahn.dev@gmail.com>
2019-05-09 01:22:08 +0900

    기본 설정

---
+++ src/main/java/com/blog/controller/PostController.java
@@ -0,0 +1,20 @@
+package com.blog.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/post")
+public class PostController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(PostController.class);
+	
+	@GetMapping(value = "/main")
+	public String main() {
+		return "post/main";
+	}
+	
+}
